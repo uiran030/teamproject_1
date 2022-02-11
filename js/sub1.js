236bf29edcfb1dff45f02cf6d1ffc28568aa5f5f
jeong_euiran <90823391+uiran030@users.noreply.github.com>
2022-02-11 16:32:07 +0900

    220211

---
+++ js/sub1.js
@@ -0,0 +1,7 @@
+$('#main .text .box ul li').on('mouseover mouseout',function(){
+    $(this).toggleClass('on')
+})
+
+$('#main .imgbox > ul> li').on('mouseover mouseout',function(){
+    $(this).toggleClass('on')
+}) 
