682dcf32d606b01d6ca1ac951ecbb4de554a4705
jeong_euiran <90823391+uiran030@users.noreply.github.com>
2022-02-23 17:27:13 +0900

    20220223

--- js/api.js
+++ js/api.js
@@ -1,12 +1,14 @@
 // isRun = 중복 작동 금지
 var isRun= false
-function abc(titName,pageno,category){
+var gdata = '';
 
-    if (isRun==true){
+function abc(titName,pageno,category){
+    if (isRun==true){ 
         return
     }
 
     isRun = true
+
     $.ajax({
         type:'GET',
         url: `https://ryukevin.herokuapp.com/http://apis.data.go.kr/1262000/CountrySafetyService/getCountrySafetyList?serviceKey=Cm6iVvqGBwjh2oznueJPtJ%2Bj2nbXtKNAP5V7ZTvwx2pnzBWlI2hFXHhkuWcOo76C6XJ6iz%2FBVrcJdrFSXsuOaQ%3D%3D&numOfRows=10&pageNo=${pageno}&title=${titName}&content=${category}`,
@@ -18,7 +20,8 @@ function abc(titName,pageno,category){
             $('#main .loading').remove()
         },
         success:function(getdata){
-            usedata(getdata)
+            gdata = getdata
+            usedata(getdata, 1)
         },
         error:function(xhr){
             alert(xhr.status + '/' + xhr.errorText)
@@ -36,37 +39,44 @@ abc(titName,startPage,category)     //함수 호출
 
 // API 데이터 호출
 function usedata(data, pno){
-
     totalCount = $(data).find('totalCount').text();
     totalCount = Math.ceil(Number(totalCount)/10)
-    // console.log(totalCount)
-    var tenCount = Math.ceil(totalCount / 10)
+
     var countryN =  $(document).find('#title').find('option:selected').val()
-    // console.log(countryN)
+
     var elem = ` <div class="imgbox"><img src="./jsimg/${countryN}.gif" alt=""></div>`
+
     elem+= `<ul class="databox">`
+
     $(data).find('item').each(function(){
         var contentName = $(this).find('content').text()
-        contentName = contentName.replace(/&nbsp;/gi,'')
+        contentName = contentName.replace(/&nbsp;/gi,'').replaceAll('다.','다.<br>').replaceAll('○',' <br>○ ').replaceAll('"',"'").replaceAll('■',' <br>■ ').replaceAll('☞',' <br>☞ ').replaceAll('※',' <br>※ ').replaceAll('음.','음.<br>').replaceAll('함.','함.<br>').replaceAll('임.','임.<br>').replaceAll('●',' <br>● ').replaceAll('▶',' <br>▶ ').replaceAll('o ',' <br>o ').replaceAll('△',' <br>△ ')
+
+        var newtext = contentName.substr(0,700)
         var contenttit = $(this).find('title').text()
+        contenttit=contenttit.replaceAll('"',"'")
         var countryNa = $(this).find('countryName').text()
-        // console.log(countryNa)
+
         if(titName===countryNa){
-            console.log(contentName)
             $(this).find('.page').addClass('on')
             elem+=`<li><h2>${contenttit}</h2>`
-            elem+=`<p>${contentName}</p>`
-            
-            // elem+=`<span class="more"><a href="#">더보기<i class="fa-solid fa-angle-right"></i></a></span></li>`
+            if (contentName.length>700){
+                elem+=`<p class="txt">${newtext}...</p>`
+                elem+=`<span class="more"><button type="button" data-content="${contentName}" data-title="${contenttit}">더보기<i class="fa-solid fa-angle-right"></i></button></span></li>`
+            }else{
+                elem+=`<p class="txt">${contentName}</p></li>`
+            }
         }
-        // console.log(contentName)
     })
     elem +=`</ul>`
 
     elem += `<div class="page">`
+
     // if (startPage > 10) {
     //     elem += `<span class="prev">이전</span>`
     // }
+
+    elem += `<div class="page">`
 
     elem += `<span>`
     for (let i = startPage; i <= totalCount && i<startPage+10; i++) {
@@ -87,17 +97,15 @@ function usedata(data, pno){
     isRun = false
 
     var txtblock = $('.databox').text()
-    // console.log(txtblock)
     var noneTitle = $('#title').find('option:selected').val()
     var noneCategory = $('#category').find('option:selected').val()
-    // console.log(displaynon)
     
     if(txtblock){
         $('.imgbox , .databox , .page').fadeIn(3000)
-    }else if(noneCategory==='카테고리 선택'&&noneTitle!=='나라이름 선택'){
+    }else if(noneCategory==='카테고리 선택'&& noneTitle!=='나라이름 선택'){
         $('#main').append(`<div class="mentbox"><div class="showbox"><p>카테고리를 선택해 주세요</p></div></div>`).fadeIn(3000)
         $('.showbox').fadeIn(500)
-    }else if(noneTitle==='나라이름 선택' &&noneCategory!=='카테고리 선택'){
+    }else if(noneTitle==='나라이름 선택' && noneCategory!=='카테고리 선택'){
         $('#main').append(`<div class="mentbox"><div class="showbox"><p>원하시는 나라를 선택해 주세요</p></div></div>`).fadeIn(3000)
         $('.showbox').fadeIn(500)
     }else if(noneTitle==='나라이름 선택' && noneCategory==='카테고리 선택'){
@@ -107,13 +115,14 @@ function usedata(data, pno){
         $('#main').append(`<div class="mentbox"><div class="showbox"><p>자료가 없습니다.</p></div></div>`).fadeIn(3000)
         $('.showbox').fadeIn(500)
     }
-    limit()
 }
+
+
 
 $('#main').on('click', '.page a',function(){
     var pno= $(this).attr('href')
     category = $('#category').find('option:selected').val()
-    $('.databox, .imgbox,.page, .mentbox').remove()
+    $('.databox, .imgbox, .page, .mentbox').remove()
     abc(titName,pno,category)
     return false
 })
@@ -123,7 +132,7 @@ $('#main').on('click', '.page .next',function(){
         startPage -= 10
     }
     category = $('#category').find('option:selected').val()
-    $('.databox,.imgbox,.page, .mentbox').remove()
+    $('.databox, .imgbox, .page, .mentbox').remove()
     abc(titName,startPage,category)
 })
 
@@ -132,7 +141,7 @@ $('#main').on('click', '.page .next',function(){
         startPage += 10
     }
     category = $('#category').find('option:selected').val()
-    $('.databox,.imgbox,.page, .mentbox').remove()
+    $('.databox, .imgbox, .page, .mentbox').remove()
     abc(titName,startPage,category)
 })
 
@@ -140,36 +149,27 @@ $('#main .btn').on('click',function(){
     titName = $('#title').find('option:selected').val()
     category = $('#category').find('option:selected').val()
     startPage=1
-    $('.databox,.imgbox,.page, .mentbox').remove()
+    $('.databox, .imgbox, .page, .mentbox').remove()
     abc(titName,startPage,category)
 })
 
-// 줄 수 맞추고 더보기
-function limit() {$('.databox >li>p').each(function(){
-    var text = $(this).text()
-    var newtext = text.substr(0,1000)
-    if (text.length<1000) {
-        $(this).html(text)
-    }else{
-        $(this).text(newtext+'...')
-        $(this).parent().append(`<span class="more"><button type="button">더보기<i class="fa-solid fa-angle-right"></i></button></span></li>`)
-    }
-})}
-
 // 더보기 클릭 시 전체화면
 $('body').on('click', '#main .databox > li > .more > button',function(){
-    // var link = $(this).find('button')
-    // console.log(imglink)
-    var contentName = $(this).find('content').text()
-    contentName = contentName.replace(/&nbsp;/gi,'')
-    var contenttit = $(this).find('title').text()
-
-    var input = `<div class="fxbox"><div class="content"><h2>${contenttit}</h2><p>${contentName}</p><button type="button">나가기</button></div></div>`
+    var newcontent = $(this).attr('data-content')
+    var newcontenttit = $(this).attr('data-title')
+    
+    var input = `<div class="fxbox"><div class="content"><h2>${newcontenttit}</h2><p>${newcontent}<button type="button">닫기</button></p></div><div class="circle"><span>닫기</span></div></div>`
     $('body').append(input)
     $('.fxbox').fadeIn(500)
 })
-$('body').on('click','.fxbox .content >button, .fxbox', function(){
+
+$('body').on('click','.fxbox .circle, .fxbox button', function(){
     $('.fxbox').fadeOut(500, function(){
         $('.fxbox').remove()
     })
 })
+
+$(document).mousemove(function(e){
+    $('.circle').css("top", e.clientY+'px');
+    $('.circle').css("left", e.clientX+'px');
+});
