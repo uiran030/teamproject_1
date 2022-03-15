cc0c0a02015515d82e8bc96388034a0496ca4d55
jeong_euiran <90823391+uiran030@users.noreply.github.com>
2022-03-15 16:39:34 +0900

    Update api.js

--- js/api.js
+++ js/api.js
@@ -11,7 +11,7 @@ function abc(titName,pageno,category){
 
     $.ajax({
         type:'GET',
-        // url: `https://euiran.herokuapp.com/http://apis.data.go.kr/1262000/CountrySafetyService/getCountrySafetyList?serviceKey=Cm6iVvqGBwjh2oznueJPtJ%2Bj2nbXtKNAP5V7ZTvwx2pnzBWlI2hFXHhkuWcOo76C6XJ6iz%2FBVrcJdrFSXsuOaQ%3D%3D&numOfRows=10&pageNo=${pageno}&title=${titName}&content=${category}`,
+        url: `https://euiran.herokuapp.com/http://apis.data.go.kr/1262000/CountrySafetyService/getCountrySafetyList?serviceKey=Cm6iVvqGBwjh2oznueJPtJ%2Bj2nbXtKNAP5V7ZTvwx2pnzBWlI2hFXHhkuWcOo76C6XJ6iz%2FBVrcJdrFSXsuOaQ%3D%3D&numOfRows=10&pageNo=${pageno}&title=${titName}&content=${category}`,
        // url: `https://ryukevin.herokuapp.com/http://apis.data.go.kr/1262000/CountrySafetyService/getCountrySafetyList?serviceKey=Cm6iVvqGBwjh2oznueJPtJ%2Bj2nbXtKNAP5V7ZTvwx2pnzBWlI2hFXHhkuWcOo76C6XJ6iz%2FBVrcJdrFSXsuOaQ%3D%3D&numOfRows=10`,
         dataType:'xml',
         beforeSend:function(){
